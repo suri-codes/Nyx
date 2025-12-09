704262bc00fe6d669e232aa164b2864ede450107
Surendra Jammishetti <suri312006@gmail.com>
2025-12-08 20:59:17 -0800

    removed hyprland

--- hosts/Khaos/configuration.nix
+++ hosts/Khaos/configuration.nix
@@ -157,11 +157,6 @@
   programs = {
     nano.enable = true;
     zsh.enable = true;
-    hyprland = {
-      enable = true;
-      xwayland.enable = true;
-      package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    };
 
     # allows normal binaries to run
     nix-ld = {
