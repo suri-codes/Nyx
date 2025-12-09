405fa265655f89e230fb6978082620c5891e4525
Surendra Jammishetti <suri312006@gmail.com>
2025-12-08 21:01:16 -0800

    removed

--- hosts/Khaos/configuration.nix
+++ hosts/Khaos/configuration.nix
@@ -71,16 +71,8 @@
     vim
     libnotify
     gnupg
-    xfce.thunar
     vulkan-tools
     lm_sensors
-    #stuff for the thing
-    libsForQt5.qt5.qtquickcontrols2
-    libsForQt5.qt5.qtgraphicaleffects
-    #swww
-    inputs.swww.packages.${pkgs.system}.swww
-
-    wl-clipboard
     screen
     networkmanagerapplet
     pamixer
