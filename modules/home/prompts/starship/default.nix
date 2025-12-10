2dfb6ba4b4195160b4dd0c6944bf8bae27f8c0b1
Surendra Jammishetti <suri312006@gmail.com>
2025-12-09 20:56:12 -0800

    .

--- modules/home/prompts/starship/default.nix
+++ modules/home/prompts/starship/default.nix
@@ -1,11 +1,12 @@
-{ lib, home, ... }: {
+{ lib, home, ... }:
+{
 
-  programs.starship = {
-    enable = true;
+  # programs.starship = {
+  #   enable = true;
 
-    settings =
-      lib.mkForce (builtins.fromTOML (builtins.readFile ./starship.toml));
-  };
+  #   settings =
+  #     lib.mkForce (builtins.fromTOML (builtins.readFile ./starship.toml));
+  # };
   # home.file.".config/starship.toml".source = lib.mkForce ./starship.toml;
 
 }
