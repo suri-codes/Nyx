f8b873a911a86abb10fb3c95ac6e63226c8f8cb7
Surendra Jammishetti <suri312006@gmail.com>
2025-12-09 21:33:12 -0800

    .

--- modules/home/tools/git.nix
+++ modules/home/tools/git.nix
@@ -1,12 +1,28 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   programs.git = {
     enable = true;
-    settings.user = {
-      name = "suri312006";
-      email = "suri312006@gmail.com";
+    settings = {
 
+      user = {
+        name = "suri312006";
+        email = "suri312006@gmail.com";
+      };
+
+      pull = {
+        rebase = false;
+      };
+
+      init = {
+        defaultBranch = "main";
+      };
     };
-    lfs = { enable = true; };
+
+    lfs = {
+      enable = true;
+    };
+
   };
+
   home.packages = [ pkgs.lazygit ];
 }
