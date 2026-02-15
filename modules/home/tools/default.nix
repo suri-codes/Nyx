67127b99304af146f55afb9a7ef0742b0b6a4197
Surendra Jammishetti <suri312006@gmail.com>
2026-02-15 14:13:02 -0800

    .

--- modules/home/tools/default.nix
+++ modules/home/tools/default.nix
@@ -4,8 +4,10 @@
   imports = [
     ./helix
     ./zellij
+    ./zk
     ./git.nix
     ./btop.nix
+
   ];
 
   # home.sessionVariables.LIBRARY_PATH = "${
---
+++ modules/home/tools/zk/default.nix
@@ -0,0 +1,15 @@
+{
+  config,
+  lib,
+  pkgs,
+}:
+{
+
+  programs.zk = {
+    enable = true;
+  };
+
+  home.file.".config/zk/config.toml".source =
+    config.lib.file.MkOutOfStoreSymlink "${config.home.HomeDirectory}/Nyx/modules/home/tools/zk/config.toml";
+
+}
